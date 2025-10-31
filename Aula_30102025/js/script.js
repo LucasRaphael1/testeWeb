4810d80ea2e07a63c7be6b159676173c3c6fa15d
LucasRaphael1 <112517549+LucasRaphael1@users.noreply.github.com>
2025-10-30 21:27:28 -0300

    Add files via upload

---
+++ Aula_30102025/js/script.js
@@ -0,0 +1,6 @@
+function ligar(){
+    document.getElementById('lampOff').src = '../img/pic_bulbon.gif';
+} 
+function desligar(){
+    document.getElementById('lampOff').src = '../img/pic_bulboff.gif'
+}
